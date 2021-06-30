34c3b834bd6227b023c5225559df8fb23448b7aa
niketanjha <niketanjha@gmail.com>
2021-06-30 19:24:51 +0530

    App created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import  React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={numbGrid:[{id:Math.random(),element:1},
+      {id:Math.random(),element:1}, {id:Math.random(),element:1}
+      ,{id:Math.random(),element:1},{id:Math.random(),element:1}
+      ,{id:Math.random(),element:1},{id:Math.random(),element:1}
+      ,{id:Math.random(),element:1},{id:Math.random(),element:1}]      
+    };
+  }
+  render(){
+    return(
+      <div className="mainDiv">
+        <div className="navBar">
+          <ul id ="navList">
+            <li ><a className="firstElement" href="#">Layout 1</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>          
+        <div className="pageHeading">
+          <h1 id="h1Heading">Page Heading</h1>
+          <h3 id="secondaryText">Secondary Text</h3>
+        </div>
+        <div className="horizontalLine">
+          <hr style={{marginLeft:"60px",marginRight:"60px"}} />
+        </div>        
+        <div className="gridMain">
+          {this.state.numbGrid.map((id,element)=>{
+            return(
+              <div className="row" key={id.id}>
+                <div className="column">
+                  <div className="card">
+                    <div className="imageGray">700 x 400</div>
+                    <h4 style={{color:'blue',marginTop:"0px"}}>Project Name</h4>
+                    <p style={{marginTop:"-10px", width:"110%",fontSize:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    Nam viverra euismod odio, 
+                    gravida pellentesque urna varius vitae.</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <footer><p>copyright &copy; your website 2014</p></footer>
+        <div className="footer">
+          <p>copyright &copy; your website 2014</p>
+        </div>
+      </div>
+      
+    );
+  }
 }
 
-export default App;
+export default App; 
