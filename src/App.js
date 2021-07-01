d17e3e1e83793df5db97c36881d8eb346ccdb69f
Niketan <niketanjha@gmail.com>
2021-07-01 15:39:58 +0530

    fixed_issues

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component{
   render(){
     return(
       <div className="mainDiv">
+
         <div className="navBar">
           <ul id ="navList">
             <li ><a className="firstElement" href="#">Layout 1</a></li>
@@ -23,32 +24,34 @@ class App extends React.Component{
             <li><a href="#">Contact</a></li>
           </ul>
         </div>          
+
         <div className="pageHeading">
-          <h1 id="h1Heading">Page Heading</h1>
-          <h3 id="secondaryText">Secondary Text</h3>
+          <h1 style={{marginBottom:"0px", fontWeight:"400" ,display:"inline-block",color:"#333333",fontFamily:"Arial, Helvetica, sans-serif"}} id="h1Heading">Page Heading</h1>
+          <span className="secondaryText">Secondary Text</span>
         </div>
+
         <div className="horizontalLine">
-          <hr style={{marginLeft:"60px",marginRight:"60px"}} />
+          <hr style={{marginLeft:"110px",marginRight:"110px"}} />
         </div>        
+
         <div className="gridMain">
           {this.state.numbGrid.map((id,element)=>{
             return(
-              <div className="row" key={id.id}>
-                <div className="column">
                   <div className="card">
                     <div className="imageGray">700 x 400</div>
-                    <h4 style={{color:'blue',marginTop:"0px"}}>Project Name</h4>
-                    <p style={{marginTop:"-10px", width:"110%",fontSize:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    <h1 style={{fontSize:"28px",fontWeight:"bold",color:"#3c81cf",marginTop:"10px",fontFamily:"djavue"}}>Project Name</h1>
+                    <p style={{marginTop:"-10px", width:"95%",fontSize:"13px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     Nam viverra euismod odio, 
                     gravida pellentesque urna varius vitae.</p>
                   </div>
-                </div>
-              </div>
             );
           })}
         </div>
-        <div className="footer1">
-          <p style={{paddingTop:"50px"}}>copyright &copy; your website 2014</p>
+        <div className="footer">
+          <div className="horizontalLine">
+          <hr style={{marginLeft:"110px",marginRight:"110px"}} />
+          </div>
+          <p style={{paddingTop:"40px",paddingBottom:"40px",paddingLeft:"110px"}}>copyright &copy; your website 2014</p>
         </div>
       </div>
       
